02904716987e05f65aab17aa0d56db7a4133a670
SimpleX-T <ntmark2004@gmail.com>
2025-02-07 16:59:36 +0100

    font editing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
 
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
@@ -10,6 +11,11 @@ export const metadata: Metadata = {
   description: "Developed by devtochukwu",
 };
 
+const poppins = Poppins({
+  weight: ["400", "500", "600"],
+  subsets: ["latin"],
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -18,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${GeistMono.variable} ${GeistSans.variable} antialiased`}
+        className={`${GeistMono.variable} ${GeistSans.variable} ${poppins.className} antialiased`}
       >
         {children}
       </body>
